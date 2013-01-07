c50337b625f9f486b68a55cb3e4f11cfce7348e0
Sean A. Hanley <yadyn@yahoo.com>
2013-01-07 14:50:29 -0600

    Initial code commit
    This is the existing v1 code I wrote previously, with minor cleanup for
    submission here.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-WPF-SearchTextBox
-=================
+# WPF Search TextBox Control #
 
 An Explorer-style search textbox control for WPF.
+
+## Details ##
+
+- Project created in Visual Studio 2010
+- .NET 4.0 Client Profile
+- Supports all native WPF themes (Aero, Luna, Royale, and Classic)
+- Licensed under [The Code Project Open License (CPOL) 1.02][1]
+    - Can be freely redistributed
+    - Can be modified
+    - Can be used in commercial software
+    - No warranty/guarantee
+
+[1]: http://www.codeproject.com/info/cpol10.aspx
---
+++ SearchTextBox/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WPF Search TextBox")]
+[assembly: AssemblyDescription("An Explorer-style search textbox control for WPF.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SearchTextBox")]
+[assembly: AssemblyCopyright("© Sean A. Hanley 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cdbf97ef-56d5-4126-8475-0ccbc9d7c1b7")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
