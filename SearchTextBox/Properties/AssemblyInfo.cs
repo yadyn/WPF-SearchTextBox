1aa37fe9fc17a3641c1abb9224466a84f56fc628
Sean A. Hanley <yadyn@yahoo.com>
2013-01-24 16:43:55 -0600

    Add theme reference settings
    Theme resources won't get loaded correctly unless the assembly defines
    theme locations.

--- SearchTextBox/Properties/AssemblyInfo.cs
+++ SearchTextBox/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
 
 [assembly: AssemblyTitle("WPF Search TextBox")]
 [assembly: AssemblyDescription("An Explorer-style search textbox control for WPF.")]
@@ -12,7 +15,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("cdbf97ef-56d5-4126-8475-0ccbc9d7c1b7")]
+[assembly: NeutralResourcesLanguageAttribute("")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
+
+[assembly: ThemeInfo(
+	ResourceDictionaryLocation.SourceAssembly, //where theme specific resource dictionaries are located
+	//(used if a resource is not found in the page, 
+	// or application resource dictionaries)
+	ResourceDictionaryLocation.None //where the generic resource dictionary is located
+	//(used if a resource is not found in the page, 
+	// app, or any theme specific resource dictionaries)
+)]
